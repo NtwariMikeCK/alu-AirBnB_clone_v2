87b6a1a464dedc89125f11953d4f7c214f151b5a
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-01-25 13:17:56 +0200

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,6 @@
--- Creates a database named hbnb_dev_db
-CREATE IF NOT EXISTS DATABASE hbnb_dev_db;
-
--- Create a new user hbnb_dev in localhost
+-- This file prepares MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Grant all privileges on hbnb_dev_db to the user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant SELECT privilege on the performance_schema to the user
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
--- Apply the changes
 FLUSH PRIVILEGES;
