b8f66c79321300de7792130d30055f7b45d3e2f2
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-01-25 13:24:10 +0200

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,6 @@
-
 -- a Mysql script to prepare
 -- MySQL server for the project
-CREATE DATABASE IF NOT EXISTS HBNB_MYSQL_DB;
-CREATE USER IF NOT EXISTS "hbnb_test"@"localhost" IDENTIFIED BY "HBNB_MYSQL_PWD";
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS "hbnb_test"@"localhost" IDENTIFIED BY "hbnb_test_pwd";
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO "hbnb_test"@"localhost";
 GRANT SELECT ON performance_schema.* TO "hbnb_test"@"localhost";
