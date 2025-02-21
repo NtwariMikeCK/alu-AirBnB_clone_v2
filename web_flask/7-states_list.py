dd41bff79c4d716e0cb5d3ce9b8723c2c9932960
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-02-21 13:13:35 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,37 +1,31 @@
 #!/usr/bin/python3
+"""Start web application with two routings
 """
-Starts a Flask web application that serves a list of states.
-"""
-
-from flask import Flask, render_template
 
 from models import storage
-
+from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states():
+@app.route('/states_list')
+def states_list():
+    """Render template with states
     """
-    Retrieves a list of states from storage and renders them in a template.
-
-    Returns:
-        HTML page displaying the list of states.
-    """
-    return render_template("7-states_list.html", states=storage.all("State").values())
+    path = '7-states_list.html'
+    states = storage.all(State)
+    # sort State object alphabetically by name
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_states=sorted_states)
 
 
 @app.teardown_appcontext
-def teardown():
-    """
-    Removes the current SQLAlchemy session to free up resources.
-
-    Args:
-        exception (Exception, optional): The exception that caused the teardown, if any.
+def app_teardown(arg=None):
+    """Clean-up session
     """
     storage.close()
 
 
-if __name__ == "__main__":
-    # Run the Flask application, making it accessible from any IP.
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
