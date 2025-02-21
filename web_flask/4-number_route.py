dab46cdadc4467e2ee393fe3d5efd4747a9cc8e1
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-02-21 11:21:35 +0200

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python3
-# A simple Flask web application.
+
+"""Script that starts a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def greet():
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Comment"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def greet_1():
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Comment"""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False):
-def c_text(text):
-    text = text.replace("_", " ")
-    return f"C {text}"
+@app.route('/c/<text>', strict_slashes=False)
+def text_route(text):
+    """Comment"""
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route("/python/<text>", strict_slashes=False)
-def python_tect(text):
-    if text:
-      text = text.replace("_", " ")
-    else:
-      text = "is cool"
-    return f"Pyhton {text}"
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def text_route_python(text="is cool"):
+    """Comment"""
+    return "Python {}".format(text.replace("_", " "))
 
 
-app.route("/number/<int:n>", strict_slashes=False)
-def is_int(n):
-    return f"{n} is a number"
+@app.route('/number/<int:n>', strict_slashes=False)
+def num_route(n):
+    """Comment"""
+    return "{} is a number".format(n)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
