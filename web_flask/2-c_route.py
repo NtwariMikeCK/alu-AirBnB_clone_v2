1bb37ec96de9b83ce9f9c62a9af663858edceca3
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-02-21 10:44:37 +0200

    Update 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""display message at route url
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def greet():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def greet_1():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False):
+def greet_2(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
