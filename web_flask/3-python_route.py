2be005ea13d35d121bc2871ea2bc3c8d056ab968
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-02-21 10:51:25 +0200

    Update 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+display message at route url
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def greet():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def greet_1():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False):
+def c_text(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_tect(text):
+    if text:
+      text = text.replace("_", " ")
+    else:
+      text = "is cool"
+    return f"Pyhton {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
