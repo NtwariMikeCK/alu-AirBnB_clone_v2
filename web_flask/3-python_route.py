4cfab11cdf7497e02283dd4a9db9e41ae05e6b0f
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-02-21 11:17:37 +0200

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-display message at route url
-"""
+
+""" display message at route url"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,22 +8,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def greet():
+    """Comment"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def greet_1():
+    """Comment"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False):
 def c_text(text):
+    """Comment"""
     text = text.replace("_", " ")
     return f"C {text}"
 
 
 @app.route("/python/<text>", strict_slashes=False)
 def python_tect(text):
+    """Comment"""
     if text:
       text = text.replace("_", " ")
     else:
