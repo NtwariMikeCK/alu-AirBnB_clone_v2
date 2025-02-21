08b8e80135e8709dc63242fa21d6b5ecfb796a23
NtwariMikeCK <169043939+NtwariMikeCK@users.noreply.github.com>
2025-02-21 11:07:34 +0200

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-"""display message at route url
+"""Start web application with two routings
 """
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def greet():
-    """display hello HBNB! for root
+@app.route('/')
+def hello():
+    """Return string when route queried
     """
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
-def greet_1():
-    """display HBNB for route /hbnb
+@app.route('/hbnb')
+def hbnb():
+    """Return string when route queried
     """
-    return "HBNB"
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False):
-def greet_2(text):
-    """display message for route /c/<text>
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Return reformatted text
     """
-    text = text.replace("_", " ")
+    text = text.replace('_', ' ')
     return f"C {text}"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
